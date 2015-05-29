d2dee8f8723cb81e1f094d85f1143bee24701e8b
renato souza <renatosvo@gmail.com>
2015-05-29 19:53:40 -0300

    removido rota raiz

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
 //app.use('/tweets', tweets);
 app.use('/mocks', mocks);
 
