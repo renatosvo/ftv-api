492178b3672aa07fb2905b514d1627ab2d979d7c
renato souza <renatosvo@gmail.com>
2015-06-12 20:36:33 -0300

    rota de inserçao de dados ok

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,24 +2,23 @@
  * Created by renat on 25/05/2015.
  */
 var express = require('express');
-var mysql = require('mysql');
 var router = express.Router();
 var cors = require('cors');
+var mysql = require('mysql');
 
-
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host     : 'us-cdbr-iron-east-02.cleardb.net',
     user     : 'b4aaa5a923988f',
     password : '2f873075',
     database : 'heroku_de1629e833c3cb8'
-});
+});*/
 
-/*var connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'renatosvo',
     password : 'hidrogenio',
     database : 'mapeamentotwitter'
-});*/
+});
 
 connection.connect(function(err){
     if(!err) {
@@ -83,6 +82,47 @@ router.get('/desc/query',cors(), function(req, res, next) {
         if (!err) {
             console.log(rows)
             res.send( rows);
+        }else{
+            res.send("Unable to connect");
+            console.error('Error while performing Query.',err);
+        }
+    });
+});
+
+
+
+router.post('/add/extra',cors(), function(req, res, next) {
+console.log("oi")
+    var nome = (req.body.nome) ? req.body.nome : null;
+    var description = (req.body.description) ? req.body.description : null;
+    var url = (req.body.url) ? req.body.url : null;
+    var tema = (req.body.tema) ? req.body.tema : null;
+    var tag1 = (req.body.tag1) ? req.body.tag1 : null;
+    var tag2 = (req.body.tag2) ? req.body.tag2 : null;
+    var iniciativa = (req.body.iniciativa) ? req.body.iniciativa : null;
+    var produto = (req.body.produto) ? req.body.produto : null;
+    var fase = (req.body.fase) ? req.body.fase : null;
+    var protagonista = (req.body.protagonista) ? !!req.body.protagonista : null;
+    var bibliografia = (req.body.bibliografia) ? req.body.bibliografia : null;
+    var evidencias = (req.body.evidencias) ? req.body.evidencias : null;
+    var fonte = (req.body.fonte) ? req.body.fonte : null;
+    var pais = (req.body.pais) ? req.body.pais : null;
+    var cidade = (req.body.cidade) ? req.body.cidade : null;
+    var info_add = (req.body.info_add) ? req.body.info_add : null;
+    var dt_criaaoo = new Date();
+    var is_tweet = (req.body.is_tweet) ? !!req.body.is_tweet : null;
+    var id_tweet = (req.body.id_tweet) ? req.body.id_tweet : null;
+
+    console.log(protagonista)
+
+    connection.query('Insert into extras (nome, description, url, tema, tag1, tag2, iniciativa ,' +
+    ' produto, fase,protagonista, bibliografia , evidencias, fonte, pais, cidade,info_add, dt_criação, is_tweet, id_tweet)' +
+    ' values (?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[nome, description, url, tema, tag1, tag2, iniciativa,
+        produto, fase,protagonista, bibliografia , evidencias, fonte, pais, cidade,info_add, dt_criaaoo, is_tweet, id_tweet] ,function(err, result) {
+
+        if (!err) {
+            console.log(result)
+            res.send(result);
         }else{
             res.send("Unable to connect");
             console.error('Error while performing Query.',err);
