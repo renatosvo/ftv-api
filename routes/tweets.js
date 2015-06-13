a0a7630a5b2faaebde3c123355056008731fdace
renato souza <renatosvo@gmail.com>
2015-06-13 14:07:36 -0300

    removido console.logs;  redirecionado banco

--- routes/tweets.js
+++ routes/tweets.js
@@ -6,19 +6,19 @@ var router = express.Router();
 var cors = require('cors');
 var mysql = require('mysql');
 
-/*var connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host     : 'us-cdbr-iron-east-02.cleardb.net',
     user     : 'b4aaa5a923988f',
     password : '2f873075',
     database : 'heroku_de1629e833c3cb8'
-});*/
+});
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'renatosvo',
     password : 'hidrogenio',
     database : 'mapeamentotwitter'
-});
+});*/
 
 connection.connect(function(err){
     if(!err) {
@@ -31,7 +31,6 @@ connection.connect(function(err){
 router.get('/tweets/total',cors(), function(req, res, next) {
     connection.query('select count(*) from tweets', function(err, rows, fields) {
         if (!err) {
-            console.log(rows)
             res.send( rows);
         }else{
             res.send("Unable to connect");
@@ -51,7 +50,6 @@ router.get('/tweets/query',cors(), function(req, res, next) {
     qtd+=page;
 
     fields = [minRet, maxRet, page, qtd];
-    console.log(fields)
 
 
     connection.query('select t.* from tweets t where t.retweets > ? and t.retweets < ? limit ?,?',[minRet, maxRet, page, qtd], function(err, rows, fields) {
@@ -80,7 +78,6 @@ router.get('/users/query',cors(), function(req, res, next) {
 router.get('/desc/query',cors(), function(req, res, next) {
     connection.query('select * from descritores ', function(err, rows, fields) {
         if (!err) {
-            console.log(rows)
             res.send( rows);
         }else{
             res.send("Unable to connect");
@@ -92,7 +89,6 @@ router.get('/desc/query',cors(), function(req, res, next) {
 
 
 router.post('/extra/add',cors(), function(req, res, next) {
-console.log("oi")
     var nome = (req.body.nome) ? req.body.nome : null;
     var description = (req.body.description) ? req.body.description : null;
     var url = (req.body.url) ? req.body.url : null;
@@ -113,7 +109,6 @@ console.log("oi")
     var is_tweet = (req.body.is_tweet) ? !!req.body.is_tweet : null;
     var id_tweet = (req.body.id_tweet) ? req.body.id_tweet : null;
 
-    console.log(protagonista)
 
     connection.query('Insert into extras (nome, description, url, tema, tag1, tag2, iniciativa ,' +
     ' produto, fase,protagonista, bibliografia , evidencias, fonte, pais, cidade,info_add, dt_criação, is_tweet, id_tweet)' +
