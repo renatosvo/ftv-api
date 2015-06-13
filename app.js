e208f26c8b36ea345854d22b1055e3fa818eeb82
renato souza <renatosvo@gmail.com>
2015-06-13 14:02:13 -0300

    adicionado rota root

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', routes);
+app.use('/', routes);
 //app.use('/users', users);
 app.use('/', tweets);
 app.use('/mocks', mocks);
